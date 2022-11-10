25de1c9d20872b1d9b870a5ebf78104f7d885878
Silvio Benvegnù <silvio.benve@gmail.com>
2022-11-10 18:03:32 +0100

    log visibile, popolato il file_name, aggiunta la data di creazione in visione tabella

--- da-report-upload/models/da_report.py
+++ da-report-upload/models/da_report.py
@@ -16,6 +16,8 @@ class DaReports(models.Model):
     _name = 'da.reports'
     _description = 'Da Reports'
 
+    create_date = fields.Datetime( string = 'Creato il', readonly = True )
+
     report      = fields.Binary( string = 'Report', required = True )
     file_name   = fields.Char( string = 'File Name', required = False )
     note        = fields.Text( string = 'Note', required = False, default = None )
@@ -23,30 +25,16 @@ class DaReports(models.Model):
     status      = fields.Char( string = 'Stato', required = True, default = 'uploaded' )
     log         = fields.Text( string = 'log', required = False, default = None )
     sale_orders = fields.Many2many( 'sale.order', 'report_sale_order_rel', 'report_id', 'sale_order_id', string = 'Ordini di vedita' )
+    log_text    = fields.Char( string="Log", compute = 'compute_log', store=False )
 
-#   def action_parse_report( self ):
-#       self.ensure_one()
-#       row = self.env['da.reports'].search( [ ( 'id', '=', self.id ) ], limit = 1 )
-#       parser = Parser.parse( self.report, self.env )
-
-#       #_logger.info( json.dumps( parser.header, indent = 4 ) )
-#       #_logger.info( json.dumps( parser.block1, indent = 4 ) )
-#       #_logger.info( json.dumps( parser.block2, indent = 4 ) )
-#       #_logger.info( json.dumps( parser.block3, indent = 4 ) )
-#       #_logger.info( json.dumps( parser.block4, indent = 4 ) )
-#       #_logger.info( json.dumps( parser.block5, indent = 4 ) )
-
-#       builder = Builder.build( parser, self.env )
-#       log = parser.logs + builder.logs
-#       orders = builder.orders
-#       sale_orders = []
-#       for order in orders:
-#           sale_orders.append( ( 4, order.id ) )
-#       row.update({
-#           'log'         : json.dumps( log ),
-#           'sale_orders' : sale_orders,
-#       })
-#       _logger.info( json.dumps( log, indent = 4 ) )
+    @api.depends('log')
+    def compute_log(self):
+        for row in self:
+            log_text = ''
+            for line in json.loads( self.log ):
+                log_text += line + "<br/>"
+            #row.log_text = json.dumps(self.log, indent=4, sort_keys=False)
+            self.log_text = log_text
 
     @api.model
     def create(self, vals):
--- da-report-upload/models/sale_order.py
+++ da-report-upload/models/sale_order.py
@@ -11,22 +11,4 @@ class SaleOrderDa(models.Model):
     indirect_partner_id = fields.Many2one( 'res.partner', string = 'Cliente Indiretto', required = False, default = None )
     title               = fields.Char( string = 'Titolo', required = False, default = None )
     num_offerta_stesi   = fields.Char( string = 'N° Offerta', required = False, default = None )
-    link                = fields.Char( compute = 'compute_link', store=False )
 
-
-    def open_form_view(self):
-        self.ensure_one()
-        form_view = self.env.ref('MODULE.XML_VIEW_ID')
-        return {
-            'name': _('Sale Order'),
-            'res_model': 'sale.order',
-            'res_id': self.id,
-            'views': [(form_view.id, 'form'), ],
-            'type': 'ir.actions.act_window',
-            #'target': 'new',
-        }
-
-    @api.depends('title')
-    def compute_link(self):
-        for row in self:
-            row.link = '/web#id=' + str( row.id ) + '&action=423&model=sale.order&view_type=form&cids=1&menu_id=259'
