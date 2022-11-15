26e6577a920da8e2b6102be5e7310f3c4b40ecd0
francescapenso <francesca.penso@gmail.com>
2022-11-15 11:01:31 +0100

    Add files via upload

---
+++ l10n_it_fatturapa_in_fix_vat_import-14.0/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "ITA - Fattura elettronica - Ricezione - FIX vat normalize",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "summary": "Ricezione fatture elettroniche",
+    "author": "Apulia Software",
+    "depends": [
+        "l10n_it_fatturapa_in",
+    ],
+    "data": ['models/wizard_import_fatturapa_view.xml'],
+    "installable": True,
+}
